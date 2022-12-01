0d872edd805d42d6e204e9f42aa45117bd5c924a
rizki-kurniawan <rizki.kurniawan02@ui.ac.id>
2022-12-02 01:42:16 +0700

    Implement todo list model

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class ToDoList(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todoList")
+    
+    def getAllTodo(self):
+        return self.todos.all()
+
+    def markDone(self, todoNames):
+        for todoName in todoNames:
+            todo = ToDo.objects.get(name=todoName)
+            if todo.todoList == self:
+                todo.markDone()
+                todo.save()
 
 class ToDo(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    isDone = models.BooleanField(default=False)
+    todoList = models.ForeignKey(ToDoList, on_delete=models.CASCADE, related_name="todos")
+
+    def markDone(self):
+       self.isDone = True
