93d73cd4ad9a4438ada4499931eec042d3079f2b
rizki-kurniawan <rizki.kurniawan02@ui.ac.id>
2022-12-02 01:41:54 +0700

    Fix unit test

--- todo_list/tests.py
+++ todo_list/tests.py
@@ -6,14 +6,14 @@ class TodoListTest(TestCase):
     
     def setUp(self):
         
-        user1 = User.objects.create_user(username="cyber-jar", password="best-card")
-        user2 = User.objects.create_user(username="blue-eyes-ultimate-dragon", password="NoNeedPass")
+        self.user1 = User.objects.create_user(username="cyber-jar", password="best-card")
+        self.user2 = User.objects.create_user(username="blue-eyes-ultimate-dragon", password="NoNeedPass")
         
-        list1 = ToDoList.objects.create(name="Empty ToDoList", owner=user1)
-        list2 = ToDoList.objects.create(name="Sunday ToDo List", owner=user2)
+        self.list1 = ToDoList.objects.create(name="Empty ToDoList", owner=self.user1)
+        self.list2 = ToDoList.objects.create(name="Sunday ToDo List", owner=self.user2)
 
-        ToDo.objects.create(name="Kill dark magician", todoList=list2)
-        ToDo.objects.create(name="Kill kuriboh", todoList=list2)
+        ToDo.objects.create(name="Kill dark magician", todoList=self.list2)
+        ToDo.objects.create(name="Kill kuriboh", todoList=self.list2)
     
     def test_todo_list_creation(self):
         emptyList = ToDoList.objects.get(name="Empty ToDoList")
@@ -21,8 +21,8 @@ class TodoListTest(TestCase):
 
         self.assertEqual("Empty ToDoList", emptyList.name)
         self.assertEqual(self.user1, emptyList.owner)
-        self.assertEqual([], emptyList.getAllTodo())
-
+        self.assertTrue(len(emptyList.getAllTodo()) == 0)
+        
         self.assertEqual("Sunday ToDo List", list2.name)
         self.assertEqual(self.user2, list2.owner)
         self.assertTrue(len(list2.getAllTodo()) > 0)
@@ -47,14 +47,21 @@ class TodoListTest(TestCase):
 
         emptyList.markDone([todo2.name])
 
-        self.assertEqual([], emptyList.getAllTodo())
+        self.assertTrue(len(emptyList.getAllTodo()) == 0)
         self.assertFalse(todo2.isDone)
 
     def test_mark_done_todo_known(self):
         list2 = ToDoList.objects.get(name="Sunday ToDo List")
-        todo1 = ToDo.objects.get(name="Kill dark magician")
         todo2 = ToDo.objects.get(name="Kill kuriboh")
 
         list2.markDone([todo2.name])
-        self.assertTrue(todo2.isDone)
-        self.assertFalse(todo1.isDone)
+
+        todo2_updated = ToDo.objects.get(name="Kill kuriboh")
+        todo1_updated = ToDo.objects.get(name="Kill dark magician")
+        
+        self.assertTrue(todo2_updated.isDone)
+        self.assertFalse(todo1_updated.isDone)
+
+        # rollback
+        todo2_updated.isDone = False
+        todo2_updated.save()
