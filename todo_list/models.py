a812e24efe50e895a460bbfe5005391acafea220
rizki-kurniawan <rizki.kurniawan02@ui.ac.id>
2022-12-02 14:22:15 +0700

    Implement Pengguna class

--- todo_list/models.py
+++ todo_list/models.py
@@ -8,12 +8,13 @@ class ToDoList(models.Model):
     def getAllTodo(self):
         return self.todos.all()
 
-    def markDone(self, todoNames):
-        for todoName in todoNames:
-            todo = ToDo.objects.get(name=todoName)
-            if todo.todoList == self:
+    def markDone(self, todoIds):
+        for todo in self.todos.all():
+            if todo.id in todoIds:
                 todo.markDone()
-                todo.save()
+            else:
+                todo.isDone=False
+            todo.save()
 
 class ToDo(models.Model):
     name = models.CharField(max_length=50)
@@ -22,3 +23,27 @@ class ToDo(models.Model):
 
     def markDone(self):
        self.isDone = True
+
+class Pengguna(User):
+    class Meta:
+        proxy = True
+    
+    def getAllTodoList(self):
+        return list(self.todoList.all())
+    
+    def getTodoList(self, todoListId):
+        try:
+            return self.todoList.filter(id=todoListId).get()
+        except:
+            return None
+    
+    def deleteTodoList(self, todoListId):
+        todoList = self.todoList.filter(id=todoListId).delete()
+        return todoList
+    
+    def createTodoList(self, todoListName, todos):
+        todoList = ToDoList.objects.create(name=todoListName, owner=self)
+        for todo in todos:
+            ToDo.objects.create(name=todo, todoList=todoList).save()
+        todoList.save()
+        return todoList
