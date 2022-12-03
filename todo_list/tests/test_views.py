4b211b6dcf3368e81442650a9e89667f061b452f
rizki-kurniawan <rizki.kurniawan02@ui.ac.id>
2022-12-03 12:55:10 +0700

    Fix view tests

--- todo_list/tests/test_views.py
+++ todo_list/tests/test_views.py
@@ -4,11 +4,13 @@ from todo_list.models import *
 from django.urls import reverse
 import json
 
+
 class TodoListPopulator(TestCase):
-    
+    password = '1X<ISRUkw+tuK'
+
     def setUp(self):
-        self.user1 = Pengguna.objects.create_user(username="a", password="bcd")
-        self.user2 = Pengguna.objects.create_user(username="b", password="def")
+        self.user1 = Pengguna.objects.create_user(username='testuser1', password=self.password)
+        self.user2 = Pengguna.objects.create_user(username="testuser2", password=self.password)
 
         self.list1 = ToDoList.objects.create(name="list1", owner=self.user1)
         self.list2 = ToDoList.objects.create(name="list2", owner=self.user1)
@@ -23,121 +25,125 @@ class TestGetTodoListsView(TodoListPopulator):
     
     def test_get_todo_lists_anonymous_user(self):
         response = self.client.get(reverse('todo_lists'))
-        self.assertRedirects(response, reverse('home'))
+        self.assertRedirects(response, '/accounts/login/?next=/todo-lists')
     
     def test_get_todo_lists_logged_in_user(self):
-        self.client.login(username=self.user1.get_username, password="bcd")
+        self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
         response = self.client.get(reverse('todo_lists'))
 
-        self.assertEqual(str(response.context['user']), 'a')
+        self.assertEqual(str(response.context['user']), 'testuser1')
         self.assertEqual(200, response.status_code)
-        self.assertTemplateUsed(response, 'todo_list/todo_lists.html')
+        self.assertTemplateUsed(response, 'todo_lists.html')
         
         self.assertEqual(2, len(response.context['todo_lists']))
-        self.assertTrue(self.list1 in response.context['todo_lists'])
-        self.assertTrue(self.list2 in response.context['todo_lists'])
+
+        self.assertEqual(self.list1.name, response.context['todo_lists'][0]['name'])
+        self.assertTrue(self.list2.name, response.context['todo_lists'][0]['name'])
 
 class TestGetToDoListDetailView(TodoListPopulator):
     
     def test_detail_todo_list_anonymous_user(self):
-        response = self.client.get(reverse('detail_todo_list', args=self.list1.id))
-        self.assertRedirects(response, reverse('home'))
+        response = self.client.get(reverse('detail_todo_list', args='1'))
+        self.assertRedirects(response, '/accounts/login/?next=/todo-list/1')
     
     def test_detail_todo_list_when_todo_list_exists(self):
-        self.client.login(username=self.user1.get_username, password="bcd")
-        response = self.client.get(reverse('detail_todo_list', args=self.list1.id))
+        self.client.login(username=self.user1.username, password=self.password)
+        response = self.client.get(reverse('detail_todo_list', args=str(self.list1.id)))
 
-        self.assertEqual(str(response.context['user']), 'a')
+        self.assertEqual(str(response.context['user']), self.user1.username)
         self.assertEqual(200, response.status_code)
 
         self.assertEqual(self.list1, response.context['todo_list'])
     
     def test_detail_todo_list_when_doesnt_exist(self):
-        self.client.login(username=self.user2.get_username, password="def")
-        response = self.client.get(reverse('detail_todo_list', args=self.list1.id))
+        self.client.login(username=self.user2.username, password=self.password)
+        response = self.client.get(reverse('detail_todo_list', args=str(self.list1.id)))
 
-        self.assertEqual(str(response.context['user']), 'b')
         self.assertEqual(404, response.status_code)
 
 class UpdateToDoList(TodoListPopulator):
     def test_update_todo_list_anonymous_user(self):
-        response = self.client.get(reverse('update_todo_list', args=self.list1.id))
-        self.assertRedirects(response, reverse('home'))
+        response = self.client.get(reverse('update_todo_list', args='1'))
+        self.assertRedirects(response, '/accounts/login/?next=/todo-list/1/update')
     
     def test_update_todo_list_for_eligible_user(self):
-        self.client.login(username=self.user1.get_username, password="bcd")
-        response = self.client.post(reverse('update_todo_list', args=self.list1.id), 
+        self.client.login(username=self.user1.username, password=self.password)
+        response = self.client.post(reverse('update_todo_list', args=str(self.list1.id)), 
                         data=json.dumps({
-                            'todos':[self.list1.id]
-                        }))
-        
+                            'todos':[self.todo1.id]
+                        }),
+                        content_type='application/json'
+                        )
         todo1 = ToDo.objects.get(id=self.todo1.id)
         todo2 = ToDo.objects.get(id=self.todo2.id)
 
         self.assertTrue(todo1.isDone)
         self.assertFalse(todo2.isDone)
 
-        self.assertEqual(str(response.context['user']), 'a')
+        self.assertEqual(str(response.context['user']), self.user1.username)
         self.assertEqual(200, response.status_code)
     
-    def test_update_todo_list_for_illegal_user(self):
-        self.client.login(username=self.user2.get_username, password="def")
-        response = self.client.post(reverse('update_todo_list', args=self.list1.id), 
+    def test_update_todo_list_for_non_existed_todo_list(self):
+        self.client.login(username=self.user2.username, password=self.password)
+        response = self.client.post(reverse('update_todo_list', args=str(self.list1.id)), 
                         data=json.dumps({
-                            'todos':[self.list2.id]
-                        }))
+                            'todos':[self.todo2.id]
+                        }),
+                        content_type='application/json'
+                        )
 
         todo2 = ToDo.objects.get(id=self.todo2.id)
         self.assertFalse(todo2.isDone)
 
-        self.assertEqual(str(response.context['user']), 'b')
-        self.assertEqual(403, response.status_code)
+        self.assertEqual(404, response.status_code)
 
-class CreateToDoListView(TestCase):
-    def setUp(self):
-        self.user1 = Pengguna.objects.create_user(username="a", password="bcd")
-        self.user2 = Pengguna.objects.create_user(username="b", password="def")
+class CreateToDoListView(TodoListPopulator):
 
     def test_create_todo_list_anonymous_user(self):
         response = self.client.get(reverse('create_todo_list'))
-        self.assertRedirects(response, reverse('home'))
+        self.assertRedirects(response, '/accounts/login/?next=/todo-lists/create')
     
-    def test_create_todo_list(self):
-        self.client.login(username=self.user1.get_username, password="bcd")
+    def test_get_create_todo_list(self):
+        self.client.login(username=self.user2.username, password=self.password)
+        response = self.client.get(reverse('create_todo_list'))
+        self.assertEqual(str(response.context['user']), self.user2.username)
+        self.assertEqual(200, response.status_code)
+
+    def test_post_create_todo_list(self):
+        self.client.login(username=self.user2.username, password=self.password)
         response = self.client.post(reverse('create_todo_list'), 
-                data=json.dump({
+                data=json.dumps({
                     'list_name':'new_list1',
                     'todo_names': ['todo1', 'todo2']
-                }))
-        user1 = Pengguna.objects.get(username='a')
-        self.assertEqual(1, len(user1.getAllTodoList()))
-        self.assertTrue('new_list1', user1.getAllTodoList()[0])
+                }),
+                content_type='application/json')
+        user2 = Pengguna.objects.get(username=self.user2.username)
+        self.assertEqual(1, len(user2.getAllTodoList()))
+        self.assertTrue('new_list1', user2.getAllTodoList()[0])
 
-        self.assertEqual(str(response.context['user']), 'a')
+        self.assertEqual(str(response.context['user']), self.user2.username)
         self.assertEqual(200, response.status_code)
 
 class DeleteToDoListView(TodoListPopulator):
     def test_delete_todo_list_anonymous_user(self):
-        response = self.client.get(reverse('delete_todo_list'))
-        self.assertRedirects(response, reverse('home'))
+        response = self.client.get(reverse('delete_todo_list', args='1'))
+        self.assertRedirects(response, '/accounts/login/?next=/todo-list/1/delete')
     
     def test_delete_existed_todo_list(self):
-        self.client.login(username=self.user1.get_username, password="bcd")
-        response = self.client.post(reverse('delete_todo_list', args=self.list1.id))
+        self.client.login(username=self.user1.username, password=self.password)
+        response = self.client.get(reverse('delete_todo_list', args=str(self.list1.id)))
         
-        user1 = Pengguna.objects.get(username='a')
-        self.assertTrue(self.list1 not in user1.getAllTodoList())
-        self.assertTrue(self.list2 in user1.getAllTodoList())
+        user1 = Pengguna.objects.get(username=self.user1.username)
+        self.assertTrue(len(user1.getAllTodoList()) == 1)
 
-        self.assertEqual(str(response.context['user']), 'a')
+        self.assertEqual(str(response.context['user']), self.user1.username)
         self.assertEqual(200, response.status_code)
     
     def test_delete_not_exist_todo_list(self):
-        self.client.login(username=self.user2.get_username, password="def")
-        response = self.client.post(reverse('delete_todo_list', args=self.list2.id))
+        self.client.login(username=self.user2.username, password=self.password)
+        response = self.client.get(reverse('delete_todo_list', args=str(self.list2.id)))
 
-        user1 = Pengguna.objects.get(username='a')
-        self.assertTrue(self.list2 in user1.getAllTodoList())
+        user1 = Pengguna.objects.get(username=self.user1.username)
+        self.assertTrue(len(user1.getAllTodoList()) == 2)
 
-        self.assertEqual(str(response.context['user']), 'b')
         self.assertEqual(404, response.status_code)
