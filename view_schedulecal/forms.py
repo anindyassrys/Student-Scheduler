0638c687a6429c7ccc928875ff595eadc887cc70
adellara <adellamadea@gmail.com>
2022-12-04 16:41:53 +0700

    Coba FE

---
+++ view_schedulecal/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import *
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model=Event
+        fields=[
+            "time",
+            "title",
+            "description"
+        ]
+
+
