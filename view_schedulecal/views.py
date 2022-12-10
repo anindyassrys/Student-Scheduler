17704cd98b195e711eb84ea554827ca0b406f364
adellara <adellamadea@gmail.com>
2022-12-10 14:45:14 +0700

    Implement Copy Link

--- view_schedulecal/views.py
+++ view_schedulecal/views.py
@@ -8,11 +8,13 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request, 'home.html')
 
+@login_required
 def listEvent(request):
     q=AppointmentRequest.objects.getAcceptedAppointmentRequests(request.user)
     e=Event.objects.all()
     return render(request, 'listEvent.html',{'q':q,'e':e})
 
+@login_required
 def detail(request,id):
     q=AppointmentRequest.objects.getAcceptedAppointmentRequests(request.user).get(pk=id)
     return render(request,'detailEvent.html',{'q':q})
@@ -26,6 +28,7 @@ def createEvent(request):
     context['form'] = form
     return render(request,"createEvent.html",context)
 
+@login_required
 def detailEvent(request,id):
     e=Event.objects.all().get(pk=id)
     return render(request,'detailEventE.html',{'e':e})
